359e72b07634e47d4e426d21114c64ea88ddf9e4
Jay Harris <jharris812@gmail.com>
2022-07-17 14:02:30 -0400

    fixes

--- Assets/Scenes/GreenButton.cs
+++ Assets/Scenes/GreenButton.cs
@@ -3,54 +3,55 @@ using System;
 
 public class GreenButton : TextureButton
 {
-	private Sprite bg;
-	private Sprite bgHover;
+    private Sprite bg;
+    private Sprite bgHover;
 
-	[Export]
-	private PackedScene nextScene;
-	private Autoload autoload;
+    [Export]
+    private PackedScene nextScene;
+    private Autoload autoload;
 
-	[Export]
-	private bool reloadCurrentScene = false;
+    [Export]
+    private bool reloadCurrentScene = false;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		autoload = GetNode<Autoload>("/root/Autoload");
-		bg = GetNode<Sprite>("bg");
-		bgHover = GetNode<Sprite>("bg-hover");
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        autoload = GetNode<Autoload>("/root/Autoload");
+        bg = GetNode<Sprite>("bg");
+        bgHover = GetNode<Sprite>("bg-hover");
+    }
 
-	private void _on_GreenButton_mouse_entered()
-	{
-		bg.Visible = false;
-		bgHover.Visible = true;
-	}
+    private void _on_GreenButton_mouse_entered()
+    {
+        bg.Visible = false;
+        bgHover.Visible = true;
+    }
 
-	private void _on_GreenButton_mouse_exited()
-	{
-		bg.Visible = true;
-		bgHover.Visible = false;
-	}
+    private void _on_GreenButton_mouse_exited()
+    {
+        bg.Visible = true;
+        bgHover.Visible = false;
+    }
 
-	private void _on_GreenButton_pressed()
-	{
-		if (reloadCurrentScene)
-		{
-			GetTree().ReloadCurrentScene();
-		}
-		else
-		{
-			if (nextScene != null)
-			{
-				GetTree().ChangeSceneTo(nextScene);
-				autoload.ResetGame();
-			}
-			else
-			{
-				GetTree().ChangeScene("res://main.tscn");
-				autoload.ResetGame();
-			}
-		}
-	}
+    private void _on_GreenButton_pressed()
+    {
+        if (reloadCurrentScene)
+        {
+            autoload.levelComplete = false;
+            GetTree().ReloadCurrentScene();
+        }
+        else
+        {
+            if (nextScene != null)
+            {
+                GetTree().ChangeSceneTo(nextScene);
+                autoload.ResetGame();
+            }
+            else
+            {
+                GetTree().ChangeScene("res://main.tscn");
+                autoload.ResetGame();
+            }
+        }
+    }
 }
