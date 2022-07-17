c52626cf45b91d40793a53bddcf56e12a930ad42
Jay Harris <jharris812@gmail.com>
2022-07-16 21:36:38 -0400

    adding action dice

--- Die_Spinner.cs
+++ Die_Spinner.cs
@@ -3,230 +3,230 @@ using System;
 
 public class Die_Spinner : Control
 {
-    private Spatial rotator;
+	private Spatial rotator;
 
-    private int currentSide = 1;
+	private int currentSide = 1;
 
-    [Export]
-    private int rotation_speed = 3;
+	[Export]
+	private int rotation_speed = 3;
 
-    [Export]
-    private PackedScene diceModel;
+	[Export]
+	private PackedScene diceModel;
 
-    public enum DieType
-    {
-        Action,
-        DoubleMovement,
-        Movement
-    }
+	public enum DieType
+	{
+		Action,
+		DoubleMovement,
+		Movement
+	}
 
-    [Export]
-    private DieType diceType = DieType.Movement;
+	[Export]
+	private DieType diceType = DieType.Movement;
 
-    [Export]
-    private int indexOffset = 0;
+	[Export]
+	private int indexOffset = 0;
 
-    private int roll_count = 0;
+	private int roll_count = 0;
 
-    private int diceNumber = 0;
+	private int diceNumber = 0;
 
-    private int cachedRemainingMoves = 0;
-    private int cachedRemainingActions = 0;
+	private int cachedRemainingMoves = 0;
+	private int cachedRemainingActions = 0;
 
-    private Player player = null;
+	private Player player = null;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        int layerIndex = 19 - (GetIndex() + indexOffset);
-        GetNode<Camera>("Die_Viewport/Camera").SetCullMaskBit(layerIndex, true);
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		int layerIndex = 19 - (GetIndex() + indexOffset);
+		GetNode<Camera>("Die_Viewport/Camera").SetCullMaskBit(layerIndex, true);
 
-        rotator = GetNode<Spatial>("Die_Viewport/Rotator");
-        MeshInstance die = (MeshInstance)diceModel.Instance();
-        die.SetLayerMaskBit(layerIndex, true);
+		rotator = GetNode<Spatial>("Die_Viewport/Rotator");
+		MeshInstance die = (MeshInstance)diceModel.Instance();
+		die.SetLayerMaskBit(layerIndex, true);
 
-        foreach (Sprite3D s in die.GetChildren())
-        {
-            s.SetLayerMaskBit(layerIndex, true);
-        }
+		foreach (Sprite3D s in die.GetChildren())
+		{
+			s.SetLayerMaskBit(layerIndex, true);
+		}
 
-        roll();
+		roll();
 
-        rotator.AddChild(die);
+		rotator.AddChild(die);
 
-        player = GetNode<Player>("/root/Spatial/Player");
-    }
+		player = GetNode<Player>("/root/Spatial/Player");
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        if (diceType == DieType.Movement || diceType == DieType.DoubleMovement)
-        {
-            if (player.remainingMoves != cachedRemainingMoves)
-            {
-                cachedRemainingMoves = player.remainingMoves;
-                var newModulate = new Color(Modulate);
-                newModulate.a = cachedRemainingMoves < 1 ? 0.3F : 1F;
-                Modulate = newModulate;
-            }
-        }
-        else if (diceType == DieType.Action)
-        {
-            if (player.remainingActions != cachedRemainingActions)
-            {
-                cachedRemainingActions = player.remainingActions;
-                var newModulate = new Color(Modulate);
-                newModulate.a = cachedRemainingActions < 1 ? 0.3F : 1F;
-                Modulate = newModulate;
-            }
-        }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		if (diceType == DieType.Movement || diceType == DieType.DoubleMovement)
+		{
+			if (player.remainingMoves != cachedRemainingMoves)
+			{
+				cachedRemainingMoves = player.remainingMoves;
+				var newModulate = new Color(Modulate);
+				newModulate.a = cachedRemainingMoves < 1 ? 0.3F : 1F;
+				Modulate = newModulate;
+			}
+		}
+		else if (diceType == DieType.Action)
+		{
+			if (player.remainingActions != cachedRemainingActions)
+			{
+				cachedRemainingActions = player.remainingActions;
+				var newModulate = new Color(Modulate);
+				newModulate.a = cachedRemainingActions < 1 ? 0.3F : 1F;
+				Modulate = newModulate;
+			}
+		}
 
-        //Rotate the die.
-        Vector3 targetRotation;
+		//Rotate the die.
+		Vector3 targetRotation;
 
-        switch (currentSide)
-        {
-            case 1:
-                targetRotation = new Vector3(0, 0, 0);
-                break;
-            case 2:
-                targetRotation = new Vector3(90, 0, 0);
-                break;
-            case 3:
-                targetRotation = new Vector3(180, 0, 0);
-                break;
-            case 4:
-                targetRotation = new Vector3(270, 0, 0);
-                break;
-            case 5:
-                targetRotation = new Vector3(0, 90, 0);
-                break;
-            case 6:
-                targetRotation = new Vector3(0, -90, 0);
-                break;
-            default:
-                targetRotation = new Vector3(0, 0, 0);
-                break;
-        }
+		switch (currentSide)
+		{
+			case 1:
+				targetRotation = new Vector3(0, 0, 0);
+				break;
+			case 2:
+				targetRotation = new Vector3(90, 0, 0);
+				break;
+			case 3:
+				targetRotation = new Vector3(180, 0, 0);
+				break;
+			case 4:
+				targetRotation = new Vector3(270, 0, 0);
+				break;
+			case 5:
+				targetRotation = new Vector3(0, 90, 0);
+				break;
+			case 6:
+				targetRotation = new Vector3(0, -90, 0);
+				break;
+			default:
+				targetRotation = new Vector3(0, 0, 0);
+				break;
+		}
 
-        var addedRotation = roll_count % 2 == 0 ? 0 : 360;
-        var goal = new Vector3(
-            targetRotation.x + addedRotation,
-            targetRotation.y + addedRotation,
-            targetRotation.z + addedRotation
-        );
+		var addedRotation = roll_count % 2 == 0 ? 0 : 360;
+		var goal = new Vector3(
+			targetRotation.x + addedRotation,
+			targetRotation.y + addedRotation,
+			targetRotation.z + addedRotation
+		);
 
-        rotator.RotationDegrees = new Vector3(rotator.RotationDegrees.LinearInterpolate(goal, rotation_speed * delta));
-    }
+		rotator.RotationDegrees = new Vector3(rotator.RotationDegrees.LinearInterpolate(goal, rotation_speed * delta));
+	}
 
-    private void _on_Die_Container_pressed()
-    {
-        if (player.remainingMoves >= 1 && !player.moveLock && !player.actionLock)
-        {
-            if (diceType == DieType.Movement)
-            {
-                handleMovementClick();
-            }
-            else if (diceType == DieType.DoubleMovement)
-            {
-                handleDoubleMovementClick();
-            }
-            else
-            {
-                handleActionClick();
-            }
-        }
-    }
+	private void _on_Die_Container_pressed()
+	{
+		if (player.remainingMoves >= 1 && !player.moveLock && !player.actionLock)
+		{
+			if (diceType == DieType.Movement)
+			{
+				handleMovementClick();
+			}
+			else if (diceType == DieType.DoubleMovement)
+			{
+				handleDoubleMovementClick();
+			}
+			else
+			{
+				handleActionClick();
+			}
+		}
+	}
 
-    private void handleMovementClick()
-    {
-        switch (currentSide)
-        {
-            case 1:
-                player._on_up_pressed();
-                break;
-            case 2:
-                player._on_down_pressed();
-                break;
-            case 3:
-                player._on_left_pressed();
-                break;
-            case 4:
-                player._on_right_pressed();
-                break;
-            case 5:
-                player._on_up_pressed();
-                break;
-            case 6:
-                player._on_up_pressed();
-                break;
-        }
-        roll();
-    }
+	private void handleMovementClick()
+	{
+		switch (currentSide)
+		{
+			case 1:
+				player._on_up_pressed();
+				break;
+			case 2:
+				player._on_down_pressed();
+				break;
+			case 3:
+				player._on_left_pressed();
+				break;
+			case 4:
+				player._on_right_pressed();
+				break;
+			case 5:
+				player._on_up_pressed();
+				break;
+			case 6:
+				player._on_up_pressed();
+				break;
+		}
+		roll();
+	}
 
-    private void handleDoubleMovementClick()
-    {
-        switch (currentSide)
-        {
-            case 1:
-                player._on_double_up_pressed();
-                break;
-            case 2:
-                player._on_double_down_pressed();
-                break;
-            case 3:
-                player._on_double_left_pressed();
-                break;
-            case 4:
-                player._on_double_right_pressed();
-                break;
-            case 5:
-                player._on_double_up_pressed();
-                break;
-            case 6:
-                player._on_double_up_pressed();
-                break;
-        }
-        roll();
-    }
+	private void handleDoubleMovementClick()
+	{
+		switch (currentSide)
+		{
+			case 1:
+				player._on_double_up_pressed();
+				break;
+			case 2:
+				player._on_double_down_pressed();
+				break;
+			case 3:
+				player._on_double_left_pressed();
+				break;
+			case 4:
+				player._on_double_right_pressed();
+				break;
+			case 5:
+				player._on_double_up_pressed();
+				break;
+			case 6:
+				player._on_double_up_pressed();
+				break;
+		}
+		roll();
+	}
 
-    private void handleActionClick()
-    {
-        switch (currentSide)
-        {
-            case 1:
-                player._on_action_heal_pressed();
-                break;
-            case 2:
-                player._on_action_shield_pressed();
-                break;
-            case 3:
-                player._on_action_light_attack_pressed();
-                break;
-            case 4:
-                player._on_action_area_attack_pressed();
-                break;
-            case 5:
-                player._on_action_range_attack_pressed();
-                break;
-            case 6:
-                player._on_action_light_attack_pressed();
-                break;
-        }
-    }
+	private void handleActionClick()
+	{
+		switch (currentSide)
+		{
+			case 1:
+				player._on_action_heal_pressed();
+				break;
+			case 2:
+				player._on_action_shield_pressed();
+				break;
+			case 3:
+				player._on_action_light_attack_pressed();
+				break;
+			case 4:
+				player._on_action_area_attack_pressed();
+				break;
+			case 5:
+				player._on_action_range_attack_pressed();
+				break;
+			case 6:
+				player._on_action_light_attack_pressed();
+				break;
+		}
+	}
 
-    public void roll()
-    {
-        roll_count++;
-        Random rnd = new Random();
-        int newSide = currentSide;
-        while (newSide == currentSide)
-        {
-            newSide = rnd.Next(1, 7);
-        }
+	public void roll()
+	{
+		roll_count++;
+		Random rnd = new Random();
+		int newSide = currentSide;
+		while (newSide == currentSide)
+		{
+			newSide = rnd.Next(1, 7);
+		}
 
-        // GD.Print("Rolled: " + newSide);
+		// GD.Print("Rolled: " + newSide);
 
-        currentSide = newSide;
-    }
+		currentSide = newSide;
+	}
 }
--- Player.cs
+++ Player.cs
@@ -3,287 +3,287 @@ using System;
 
 public class Player : RigidBody
 {
-    private Autoload autoload;
-    private int gridSize;
+	private Autoload autoload;
+	private int gridSize;
 
-    private Vector3 targetPos;
+	private Vector3 targetPos;
 
-    public int remainingActions;
-    public int remainingMoves;
+	public int remainingActions;
+	public int remainingMoves;
 
-    public bool moveLock = false;
-    public bool actionLock = false;
+	public bool moveLock = false;
+	public bool actionLock = false;
 
-    [Export]
-    private int maxHealth = 5;
+	[Export]
+	private int maxHealth = 5;
 
-    [Export]
-    private int slideSpeed = 5;
+	[Export]
+	private int slideSpeed = 5;
 
-    [Export]
-    private int defaultActionsPerTurn = 1;
+	[Export]
+	private int defaultActionsPerTurn = 1;
 
-    [Export]
-    public int defaultMovesPerTurn = 2;
+	[Export]
+	public int defaultMovesPerTurn = 2;
 
-    [Export]
-    private int lightDamage = 2;
+	[Export]
+	private int lightDamage = 2;
 
-    [Export]
-    private int aoeDamage = 1;
+	[Export]
+	private int aoeDamage = 1;
 
-    [Export]
-    private int rangeDamage = 1;
+	[Export]
+	private int rangeDamage = 1;
 
-    public override void _Ready()
-    {
-        autoload = GetNode<Autoload>("/root/Autoload");
-        gridSize = autoload.gridSize;
-        targetPos = Translation;
+	public override void _Ready()
+	{
+		autoload = GetNode<Autoload>("/root/Autoload");
+		gridSize = autoload.gridSize;
+		targetPos = Translation;
 
-        remainingActions = defaultActionsPerTurn;
-        remainingMoves = defaultMovesPerTurn;
-    }
+		remainingActions = defaultActionsPerTurn;
+		remainingMoves = defaultMovesPerTurn;
+	}
 
-    public override void _Process(float delta)
-    {
-        Label turnLabel = GetNode<Label>("CanvasLayer/FullScreen/Turn");
-        if (autoload.isPlayerTurn)
-        {
-            turnLabel.Text = "PLAYER TURN";
-        }
-        else
-        {
-            turnLabel.Text = "ENEMY TURN";
-        }
+	public override void _Process(float delta)
+	{
+		Label turnLabel = GetNode<Label>("CanvasLayer/FullScreen/Turn");
+		if (autoload.isPlayerTurn)
+		{
+			turnLabel.Text = "PLAYER TURN";
+		}
+		else
+		{
+			turnLabel.Text = "ENEMY TURN";
+		}
 
-        if (moveLock)
-        {
-            Translation = Translation.LinearInterpolate(targetPos, slideSpeed * delta);
+		if (moveLock)
+		{
+			Translation = Translation.LinearInterpolate(targetPos, slideSpeed * delta);
 
-            if (Translation.DistanceSquaredTo(targetPos) <= 0.001)
-            {
-                Translation = targetPos;
-                moveLock = false;
-            }
-        }
+			if (Translation.DistanceSquaredTo(targetPos) <= 0.001)
+			{
+				Translation = targetPos;
+				moveLock = false;
+			}
+		}
 
-        if (autoload.isPlayerTurn && !actionLock && remainingActions == 0 && remainingMoves == 0)
-        {
-            autoload.finishPlayerTurn();
-        }
-    }
+		if (autoload.isPlayerTurn && !actionLock && remainingActions == 0 && remainingMoves == 0)
+		{
+			autoload.finishPlayerTurn();
+		}
+	}
 
-    public void startTurn()
-    {
-        remainingActions = defaultActionsPerTurn;
-        remainingMoves = defaultMovesPerTurn;
-    }
+	public void startTurn()
+	{
+		remainingActions = defaultActionsPerTurn;
+		remainingMoves = defaultMovesPerTurn;
+	}
 
-    private void kill()
-    {
-        Sleeping = false;
-    }
+	private void kill()
+	{
+		Sleeping = false;
+	}
 
-    private void handle_move(Vector3 offset)
-    {
-        if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingMoves > 0)
-        {
-            targetPos = new Vector3(Translation.x + offset.x, Translation.y, Translation.z + offset.z);
-            moveLock = true;
-            remainingMoves--;
-        }
-    }
+	private void handle_move(Vector3 offset)
+	{
+		if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingMoves > 0)
+		{
+			targetPos = new Vector3(Translation.x + offset.x, Translation.y, Translation.z + offset.z);
+			moveLock = true;
+			remainingMoves--;
+		}
+	}
 
-    public void _on_up_pressed()
-    {
-        handle_move(new Vector3(0, 0, 0 - gridSize));
-    }
+	public void _on_up_pressed()
+	{
+		handle_move(new Vector3(0, 0, 0 - gridSize));
+	}
 
-    public void _on_left_pressed()
-    {
-        handle_move(new Vector3(0 - gridSize, 0, 0));
-    }
+	public void _on_left_pressed()
+	{
+		handle_move(new Vector3(0 - gridSize, 0, 0));
+	}
 
-    public void _on_down_pressed()
-    {
-        handle_move(new Vector3(0, 0, gridSize));
-    }
+	public void _on_down_pressed()
+	{
+		handle_move(new Vector3(0, 0, gridSize));
+	}
 
-    public void _on_right_pressed()
-    {
-        handle_move(new Vector3(gridSize, 0, 0));
-    }
+	public void _on_right_pressed()
+	{
+		handle_move(new Vector3(gridSize, 0, 0));
+	}
 
-    public void _on_double_up_pressed()
-    {
-        handle_move(new Vector3(0, 0, 0 - 2 * gridSize));
-    }
+	public void _on_double_up_pressed()
+	{
+		handle_move(new Vector3(0, 0, 0 - 2 * gridSize));
+	}
 
-    public void _on_double_left_pressed()
-    {
-        handle_move(new Vector3(0 - 2 * gridSize, 0, 0));
-    }
+	public void _on_double_left_pressed()
+	{
+		handle_move(new Vector3(0 - 2 * gridSize, 0, 0));
+	}
 
-    public void _on_double_down_pressed()
-    {
-        handle_move(new Vector3(0, 0, 2 * gridSize));
-    }
+	public void _on_double_down_pressed()
+	{
+		handle_move(new Vector3(0, 0, 2 * gridSize));
+	}
 
-    public void _on_double_right_pressed()
-    {
-        handle_move(new Vector3(2 * gridSize, 0, 0));
-    }
+	public void _on_double_right_pressed()
+	{
+		handle_move(new Vector3(2 * gridSize, 0, 0));
+	}
 
 
-    private void handle_action(float waitTime = 1.5F)
-    {
-        Timer actionTimer = GetNode<Timer>("ActionLock");
-        actionLock = true;
-        actionTimer.WaitTime = waitTime;
-        actionTimer.Start();
-    }
+	private void handle_action(float waitTime = 1.5F)
+	{
+		Timer actionTimer = GetNode<Timer>("ActionLock");
+		actionLock = true;
+		actionTimer.WaitTime = waitTime;
+		actionTimer.Start();
+	}
 
-    public void _on_action_heal_pressed()
-    {
-        if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingActions > 0)
-        {
-            autoload.playerHealth = Math.Min(maxHealth, autoload.playerHealth + 1);
-            GetNode<Particles>("Health_Particles").Emitting = true;
-            handle_action();
-            remainingActions--;
-        }
-    }
+	public void _on_action_heal_pressed()
+	{
+		if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingActions > 0)
+		{
+			autoload.playerHealth = Math.Min(maxHealth, autoload.playerHealth + 1);
+			GetNode<Particles>("Health_Particles").Emitting = true;
+			handle_action();
+			remainingActions--;
+		}
+	}
 
-    public void _on_action_shield_pressed()
-    {
-        if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingActions > 0)
-        {
-            autoload.playerShield++;
-            GetNode<Particles>("Shield_Particles").Emitting = true;
-            handle_action();
-            remainingActions--;
-        }
-    }
+	public void _on_action_shield_pressed()
+	{
+		if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingActions > 0)
+		{
+			autoload.playerShield++;
+			GetNode<Particles>("Shield_Particles").Emitting = true;
+			handle_action();
+			remainingActions--;
+		}
+	}
 
-    public void _on_action_light_attack_pressed()
-    {
-        if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingActions > 0)
-        {
-            Particles ball = GetNode<Particles>("Light_Attack_Particles");
+	public void _on_action_light_attack_pressed()
+	{
+		if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingActions > 0)
+		{
+			Particles ball = GetNode<Particles>("Light_Attack_Particles");
 
-            Godot.Collections.Array enemies = GetTree().GetNodesInGroup("enemy");
+			Godot.Collections.Array enemies = GetTree().GetNodesInGroup("enemy");
 
-            if (enemies.Count > 0)
-            {
-                Enemy closestEnemy = (Enemy)enemies[0];
-                float closestDist = Translation.DistanceSquaredTo(closestEnemy.Translation);
+			if (enemies.Count > 0)
+			{
+				Enemy closestEnemy = (Enemy)enemies[0];
+				float closestDist = Translation.DistanceSquaredTo(closestEnemy.Translation);
 
-                foreach (Enemy e in enemies)
-                {
-                    if (!e.isDead)
-                    {
-                        float enemDist = Translation.DistanceSquaredTo(e.Translation);
-                        if (enemDist < closestDist)
-                        {
-                            closestEnemy = e;
-                            closestDist = enemDist;
-                        }
-                    }
-                }
+				foreach (Enemy e in enemies)
+				{
+					if (!e.isDead)
+					{
+						float enemDist = Translation.DistanceSquaredTo(e.Translation);
+						if (enemDist < closestDist)
+						{
+							closestEnemy = e;
+							closestDist = enemDist;
+						}
+					}
+				}
 
-                if (closestDist <= 6)
-                {
-                    ball.Translation = new Vector3(closestEnemy.Translation.x - Translation.x, 2, closestEnemy.Translation.z - Translation.z);
-                    ball.Emitting = true;
-                    closestEnemy.damage(lightDamage);
-                }
-            }
+				if (closestDist <= 6)
+				{
+					ball.Translation = new Vector3(closestEnemy.Translation.x - Translation.x, 2, closestEnemy.Translation.z - Translation.z);
+					ball.Emitting = true;
+					closestEnemy.damage(lightDamage);
+				}
+			}
 
-            handle_action();
-            remainingActions--;
-            return;
-        }
-    }
+			handle_action();
+			remainingActions--;
+			return;
+		}
+	}
 
-    public void _on_action_area_attack_pressed()
-    {
-        if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingActions > 0)
-        {
-            Particles aoe = GetNode<Particles>("AOE_Attack_Particles");
+	public void _on_action_area_attack_pressed()
+	{
+		if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingActions > 0)
+		{
+			Particles aoe = GetNode<Particles>("AOE_Attack_Particles");
 
-            Godot.Collections.Array enemies = GetTree().GetNodesInGroup("enemy");
-            if (enemies.Count > 0)
-            {
-                aoe.Emitting = true;
-                foreach (Enemy e in enemies)
-                {
-                    if (!e.isDead)
-                    {
-                        float dist = Translation.DistanceSquaredTo(e.Translation);
+			Godot.Collections.Array enemies = GetTree().GetNodesInGroup("enemy");
+			if (enemies.Count > 0)
+			{
+				aoe.Emitting = true;
+				foreach (Enemy e in enemies)
+				{
+					if (!e.isDead)
+					{
+						float dist = Translation.DistanceSquaredTo(e.Translation);
 
-                        if (dist <= 6)
-                        {
-                            e.damage(aoeDamage);
-                        }
-                    }
-                }
-            }
+						if (dist <= 6)
+						{
+							e.damage(aoeDamage);
+						}
+					}
+				}
+			}
 
-            handle_action();
-            remainingActions--;
-            return;
-        }
-    }
+			handle_action();
+			remainingActions--;
+			return;
+		}
+	}
 
-    public void _on_action_range_attack_pressed()
-    {
-        if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingActions > 0)
-        {
-            Range_Attack_Particles fireBall = GetNode<Range_Attack_Particles>("Range_Attack_Particles");
+	public void _on_action_range_attack_pressed()
+	{
+		if (autoload.isPlayerTurn && !moveLock && !actionLock && remainingActions > 0)
+		{
+			Range_Attack_Particles fireBall = GetNode<Range_Attack_Particles>("Range_Attack_Particles");
 
-            Godot.Collections.Array enemies = GetTree().GetNodesInGroup("enemy");
+			Godot.Collections.Array enemies = GetTree().GetNodesInGroup("enemy");
 
-            if (enemies.Count > 0)
-            {
-                Enemy closestEnemy = (Enemy)enemies[0];
-                float closestDist = Translation.DistanceSquaredTo(closestEnemy.Translation);
+			if (enemies.Count > 0)
+			{
+				Enemy closestEnemy = (Enemy)enemies[0];
+				float closestDist = Translation.DistanceSquaredTo(closestEnemy.Translation);
 
-                foreach (Enemy e in enemies)
-                {
-                    if (!e.isDead)
-                    {
-                        float enemDist = Translation.DistanceSquaredTo(e.Translation);
-                        if (enemDist < closestDist)
-                        {
-                            closestEnemy = e;
-                            closestDist = enemDist;
-                        }
-                    }
-                }
+				foreach (Enemy e in enemies)
+				{
+					if (!e.isDead)
+					{
+						float enemDist = Translation.DistanceSquaredTo(e.Translation);
+						if (enemDist < closestDist)
+						{
+							closestEnemy = e;
+							closestDist = enemDist;
+						}
+					}
+				}
 
-                if (closestDist <= 36)
-                {
-                    fireBall.castTo(new Vector3(closestEnemy.Translation.x - Translation.x, 2, closestEnemy.Translation.z - Translation.z));
-                    closestEnemy.damage(rangeDamage);
-                }
-            }
+				if (closestDist <= 36)
+				{
+					fireBall.castTo(new Vector3(closestEnemy.Translation.x - Translation.x, 2, closestEnemy.Translation.z - Translation.z));
+					closestEnemy.damage(rangeDamage);
+				}
+			}
 
-            handle_action();
-            remainingActions--;
-            return;
-        }
-    }
+			handle_action();
+			remainingActions--;
+			return;
+		}
+	}
 
-    private void _on_end_turn_pressed()
-    {
-        autoload.finishPlayerTurn();
-        remainingActions = 0;
-        remainingMoves = 0;
-    }
+	private void _on_end_turn_pressed()
+	{
+		autoload.finishPlayerTurn();
+		remainingActions = 0;
+		remainingMoves = 0;
+	}
 
-    private void _on_ActionLock_timeout()
-    {
-        actionLock = false;
-    }
+	private void _on_ActionLock_timeout()
+	{
+		actionLock = false;
+	}
 }
